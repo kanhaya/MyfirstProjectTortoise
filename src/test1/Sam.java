430fcf63b4f93bcddb3e27b08959b8ecccbafe39
kanhaya <croissance.kanhaya@gmail.com>
2014-05-20 16:55:54 +0530

    second change

--- src/test1/Sam.java
+++ src/test1/Sam.java
@@ -5,6 +5,7 @@ public class Sam {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("My first push into git hub");
-	}
 
+System.out.println("secod line commit");
+	}
 }
